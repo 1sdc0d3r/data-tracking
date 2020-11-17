5923ed1b5430d97b08181dcf0531b8cc184baebc
Braden Bell <fish0859@gmail.com>
2020-11-16 17:03:54 -0700

    added sort function for days

--- converter.py
+++ converter.py
@@ -20,14 +20,26 @@ class Day:
 
 data = []
 
+# load in csv file
+with open(path) as fp:
+    reader = csv.reader(fp, delimiter=",")
+    # del[data[0]]
+    for row in reader:
+        data.append(Day(row[0], row[1], row[2], row[3], row[4]))
 
-def dataReader():
-    with open(path) as fp:
-        reader = csv.reader(fp, delimiter=",")
-        for row in reader:
-            data.append(Day(row[0], row[1], row[2], row[3], row[4],))
+del data[0]
+# sort data by date w/ range at 1 to keep headers
+for j in range(1, len(data) - 1):
+    for i in range(1, len(data) - 1):
+        if data[i].date > data[i+1].date:
+            data[i], data[i+1] = data[i+1], data[i]
 
 
-dataReader()
-print(data[1])
-# print
+for day in data:
+    # day.up = int(float(day.up)*.001)
+    # day.down = int(float(day.down)*.001)
+    # day.total = int(float(day.total)*.001)
+    print(day)
+    print(int(float(day.up)))
+    # print(day.total)
+    # print(data[1])
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+# for j in range(0, len(data) - 1):
+#     for i in range(0, len(data) - 1):
+#         if data[i].date > data[i+1].date:
+#             data[i], data[i+1] = data[i+1], data[i]
